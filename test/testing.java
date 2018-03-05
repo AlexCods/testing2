d322a85d5869adc68a7f43401a5f4105e2b96a3d
Alex <alexalji@gmail.com>
2018-03-05 18:07:32 +0100

    Programa terminado

--- test/testing.java
+++ test/testing.java
@@ -17,7 +17,49 @@ import org.junit.Test;
 public class testing {
     
     @Test
-    public void ComparandoCuadradoRectangulo(){
+    public void CalculandoAreaCuadrado(){
+        Figura cuadrado = new Cuadrado("cuadrado",10);
+        Double result = cuadrado.getArea();
+        Assert.assertEquals(100.0,100.0, result);
+    }
+    
+    @Test
+    public void CalculandoAreaCirculo(){
+        Figura circulo = new Circulo("circulo",5);
+        Double result = circulo.getArea();
+        Assert.assertEquals(78.54,78.54, result);
+    }
+    
+    @Test
+    public void CalculandoAreaRectangulo(){
+        Figura rectangulo = new Rectangulo("rectangulo",10,10);
+        Double result = rectangulo.getArea();
+        Assert.assertEquals(100.0,100.0, result);
+    }
+    
+    @Test
+    public void CalculandoPerimetroCuadrado(){
+        Figura cuadrado = new Cuadrado("cuadrado",10);
+        Double result = cuadrado.getPerimetro();
+        Assert.assertEquals(40.0,40.0, result);
+    }
+    
+    @Test
+    public void CalculandoPerimetroCirculo(){
+        Figura circulo = new Circulo("circulo",5);
+        Double result = circulo.getPerimetro();
+        Assert.assertEquals(31.41,31.41, result);
+    }
+    
+    @Test
+    public void CalculandoPerimetroRectangulo(){
+        Figura rectangulo = new Rectangulo("rectangulo",10,10);
+        Double result = rectangulo.getPerimetro();
+        Assert.assertEquals(40.0,40.0, result);
+    }
+    
+    @Test
+    public void ComparandoCuadradoRectanguloArea(){
         Figura rectangulo = new Rectangulo("rectangulo",10,10);
         Figura cuadrado = new Cuadrado("cuadrado",10);
         String result = Comparador.comparaArea(rectangulo, cuadrado);
@@ -25,7 +67,7 @@ public class testing {
     }
     
     @Test
-    public void ComparandoCuadradoCirculo(){
+    public void ComparandoCuadradoCirculoArea(){
         Figura cuadrado = new Cuadrado("cuadrado",10);
         Figura circulo = new Circulo("circulo",5);
         String result = Comparador.comparaArea(cuadrado, circulo);
@@ -33,10 +75,34 @@ public class testing {
     }
     
     @Test
-    public void ComparandoRectanguloCirculo(){
+    public void ComparandoRectanguloCirculoArea(){
+        Figura rectangulo = new Rectangulo("rectangulo",10,10);
+        Figura circulo = new Circulo("circulo",5);
+        String result = Comparador.comparaArea(rectangulo, circulo);
+        Assert.assertEquals("f1 es mayor", result);
+    }
+    
+    @Test
+    public void ComparandoCuadradoRectanguloPerimetro(){
+        Figura rectangulo = new Rectangulo("rectangulo",10,10);
+        Figura cuadrado = new Cuadrado("cuadrado",10);
+        String result = Comparador.comparaArea(rectangulo, cuadrado);
+        Assert.assertEquals("son iguales", result);
+    }
+    
+    @Test
+    public void ComparandoCuadradoCirculoPerimetro(){
         Figura cuadrado = new Cuadrado("cuadrado",10);
         Figura circulo = new Circulo("circulo",5);
         String result = Comparador.comparaArea(cuadrado, circulo);
+        Assert.assertEquals("f1 es mayor", result);
+    }
+    
+    @Test
+    public void ComparandoRectanguloCirculoPerimetro(){
+        Figura rectangulo = new Rectangulo("rectangulo",10,10);
+        Figura circulo = new Circulo("circulo",5);
+        String result = Comparador.comparaArea(rectangulo, circulo);
         Assert.assertEquals("f1 es mayor", result);
     }
     
